8bf219276f819bf33047709245977ca90f9e44a4
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-02 21:29:14 +0200

    add a simple printer

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,9 @@
 fun main() {
-
+    println("""
+        I love animals!
+        Let's check on the animals...
+        The deer looks fine.
+        The bat looks happy.
+        The lion looks healthy.
+    """.trimIndent())
 }
