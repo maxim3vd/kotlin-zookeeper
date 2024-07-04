faad221f52871ada7b766dc871e1ba5916c064a6
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-04 14:31:48 +0200

    Store the habitat selected by the user into an animal variable Print the animal selected

--- src/Main.kt
+++ src/Main.kt
@@ -3,4 +3,7 @@ import animals
 fun main() {
     print("Please enter the number of the habitat you would like to view: ")
     val numHabitat: Int = readln().toInt()
+
+    val animal: String = animals[numHabitat]
+    println("\n$animal")
 }
