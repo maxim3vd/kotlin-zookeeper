c5d357f723658f6660405dd9c7d1dfecfdd04a68
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-04 14:15:08 +0200

    Create an Animals.kt file Import animals variable from Animals.kt into Main.kt file

--- src/Main.kt
+++ src/Main.kt
@@ -1,25 +1,5 @@
-val camel = """
-    Switching on the camera in the camel habitat...
-     ___.-''''-.
-    /___  @    |
-    ',,,,.     |         _.'''''''._
-         '     |        /           \
-         |     \    _.-'             \
-         |      '.-'                  '-.
-         |                               ',
-         |                                '',
-          ',,-,                           ':;
-               ',,| ;,,                 ,' ;;
-                  ! ; !'',,,',',,,,'!  ;   ;:
-                 : ;  ! !       ! ! ;  ;   :;
-                 ; ;   ! !      ! !  ; ;   ;,
-                ; ;    ! !     ! !   ; ;
-                ; ;    ! !    ! !     ; ;
-               ;,,      !,!   !,!     ;,;
-               /_I      L_I   L_I     /_I
-    Look at that! Our little camel is sunbathing!
-""".trimIndent()
+import animals
 
 fun main() {
-    println(camel)
+
 }
