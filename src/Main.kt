01ab46ba25c299354f4ed7b034f4febf2d92bfdc
maxim3andr3 <maxim3.vd@gmail.com>
2024-07-18 20:34:56 +0200

    Implement loop to end only when user enters 'exit' instead of an integer

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,16 @@
 import animals
 
 fun main() {
-    print("Please enter the number of the habitat you would like to view: ")
-    val userChoice: String = readln()
-    val numHabitat: Int? = userChoice.toIntOrNull()
+    do {
+        print("Please enter the number of the habitat you would like to view: ")
+        val userChoice: String = readln()
+        val numHabitat: Int? = userChoice.toIntOrNull()
 
-    if (numHabitat != null) {
-        val animal: String = animals[numHabitat]
-        println("\n$animal")
-    }
+        if (numHabitat != null) {
+            val animal: String = animals[numHabitat]
+            println("\n$animal")
+        }
+    } while (numHabitat != null)
 
     println("See you later!")
 }
